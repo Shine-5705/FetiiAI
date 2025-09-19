d1b322e3351aa964e34029c53738223eb4a397cc
Parth Sinha <sinhaparth555@gmail.com>
2025-09-19 22:43:06 +0100

    udpated

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ def main():
     demo.launch(
         server_name="127.0.0.1",
         server_port=7860,
-        share=False,
+        share=True,
         show_api=False,
         inbrowser=True
     )
